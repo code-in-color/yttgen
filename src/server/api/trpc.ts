2b3ec93a40256f9d877a6c85143a09dc1f7ea073
William Shepherd <williamrshepherd@gmail.com>
2023-03-04 11:01:43 -0600

    Migrate to supabase auth

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -14,15 +14,14 @@
  *
  * These allow you to access things when processing a request, like the database, the session, etc.
  */
-import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import { type Session } from "next-auth";
+import { type CreateNextContextOptions } from '@trpc/server/adapters/next'
 
-import { getServerAuthSession } from "~/server/auth";
-import { prisma } from "~/server/db";
+import { prisma } from '@server/db'
+import type { Session } from '@supabase/auth-helpers-react'
 
 type CreateContextOptions = {
-  session: Session | null;
-};
+  session: Session | null
+}
 
 /**
  * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
@@ -37,9 +36,9 @@ type CreateContextOptions = {
 const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
-    prisma,
-  };
-};
+    prisma
+  }
+}
 
 /**
  * This is the actual context you will use in your router. It will be used to process every request
@@ -48,73 +47,57 @@ const createInnerTRPCContext = (opts: CreateContextOptions) => {
  * @see https://trpc.io/docs/context
  */
 export const createTRPCContext = async (opts: CreateNextContextOptions) => {
-  const { req, res } = opts;
-
-  // Get the session from the server using the getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
+  const { req, res } = opts
+  const supabaseServerClient = createServerSupabaseClient<Database>({
+    req,
+    res
+  })
+  const {
+    data: { session }
+  } = await supabaseServerClient.auth.getSession()
 
   return createInnerTRPCContext({
-    session,
-  });
-};
+    session
+  })
+}
 
 /**
  * 2. INITIALIZATION
  *
  * This is where the tRPC API is initialized, connecting the context and transformer.
  */
-import { initTRPC, TRPCError } from "@trpc/server";
-import superjson from "superjson";
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { initTRPC, TRPCError } from '@trpc/server'
+import superjson from 'superjson'
+import type { Database } from '@common/database.types'
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
   errorFormatter({ shape }) {
-    return shape;
-  },
-});
+    return shape
+  }
+})
 
 /**
- * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
- *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
- * "/src/server/api/routers" directory.
- */
-
-/**
- * This is how you create new routers and sub-routers in your tRPC API.
- *
  * @see https://trpc.io/docs/router
  */
-export const createTRPCRouter = t.router;
+export const createTRPCRouter = t.router
 
-/**
- * Public (unauthenticated) procedure
- *
- * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
- * guarantee that a user querying is authorized, but you can still access user session data if they
- * are logged in.
- */
-export const publicProcedure = t.procedure;
+export const publicProcedure = t.procedure
 
 /** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
   if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
   }
   return next({
     ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
-});
+      session: { ...ctx.session, user: ctx.session.user }
+    }
+  })
+})
 
 /**
- * Protected (authenticated) procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
- *
  * @see https://trpc.io/docs/procedures
  */
-export const protectedProcedure = t.procedure.use(enforceUserIsAuthed);
+export const protectedProcedure = t.procedure.use(enforceUserIsAuthed)
