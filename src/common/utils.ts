07f62258f2bf3a4b5b6bb1ce5248d2096190275b
William Shepherd <williamrshepherd@gmail.com>
2023-03-11 12:30:41 -0600

    Use vercel url when on preview deploymenets

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,14 @@
 export const getBaseUrl = () => {
-  if (typeof window !== 'undefined') return '' // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+  // Use preview deployment URL on preview branches
+  if (process.env.VERCEL_ENV === 'preview' && typeof window !== 'undefined')
+    return 'https://${process.env.VERCEL_URL}'
+
+  // Use relative url in browser
+  if (typeof window !== 'undefined') return
+
+  // Use vercel url in SSR mode on Vercel
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+
+  // Use localhost in SSR mode locally
+  return `http://localhost:${process.env.PORT ?? 3000}`
 }
