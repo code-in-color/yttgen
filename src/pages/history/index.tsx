0f2d0194c08f0b40f106c0ab2885c03804f2ac5f
William Shepherd <williamrshepherd@gmail.com>
2023-03-18 13:37:43 -0500

    Protect history pages

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,7 +1,18 @@
 import { api } from '@server/api/api'
+import { useUser } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const History = () => {
   const titleHistory = api.youtube.getTitleHistory.useQuery()
+  const user = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!user) {
+      void router.replace('/login')
+    }
+  })
 
   if (titleHistory) {
     return (
