ad39ccb67bf9bc388fa3648e54960226204501f0
William Shepherd <williamrshepherd@gmail.com>
2023-04-09 16:57:05 -0500

    Error out when profile is null

--- src/pages/[handle]/index.tsx
+++ src/pages/[handle]/index.tsx
@@ -1,6 +1,7 @@
-import type { GeneratedTitle, Profile } from '@prisma/client'
+import type { Post, Profile } from '@prisma/client'
 import { prisma } from '@server/db'
 import type { GetServerSidePropsContext } from 'next'
+import Link from 'next/link'
 import superjson from 'superjson'
 
 interface Props {
@@ -8,18 +9,35 @@ interface Props {
 }
 
 const ProfilePage: React.FC<Props> = ({ data }) => {
-  const userProfile = superjson.parse<
-    Profile & { generatedTitles: GeneratedTitle[] }
-  >(data)
-  const { firstName, lastName } = userProfile
-  const titles = userProfile.generatedTitles
+  console.log('userProfile', data)
+  const userProfile = superjson.parse<Profile & { posts: Post[] }>(data)
+
+  const { handle, posts } = userProfile
+
   return (
     <>
-      <p>{firstName}</p>
-      <p>{lastName}</p>
+      <p>{handle}</p>
+      <p>figuring shit out</p>
+      <ul>
+        <li></li>
+        <Link href="https://youtube.com/c/iamwillshepherd">YouTube</Link>
+        <li>
+          <Link href="https://twitch.tv/codeincolor">Twitch</Link>
+        </li>
+        <li>
+          <Link href="https://linkin/in/williamrshepherd">LinkedIn</Link>
+        </li>
+        <li>
+          <Link href="http://instagram/iamwillshepherd">Instagram</Link>
+        </li>
+      </ul>
       <ol>
-        {titles.map((title) => (
-          <li key={title.id}>{title.title}</li>
+        {posts.map((post) => (
+          <li className="border border-blue-400 px-4 py-2" key={post.id}>
+            <a href={post.contentUrl} target="_blank" rel="noreferrer">
+              {post.title}
+            </a>
+          </li>
         ))}
       </ol>
     </>
@@ -42,9 +60,18 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
       handle: handle
     },
     include: {
-      generatedTitles: true
+      posts: true
     }
   })
+
+  if (!profile) {
+    return {
+      redirect: {
+        destination: '/error',
+        permanent: false
+      }
+    }
+  }
 
   return {
     props: {
